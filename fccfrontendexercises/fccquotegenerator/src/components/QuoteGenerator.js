792d65f1779d777e0f5f1fadaa4d12d61ee00cdc
nadelacruz <nicoangelodelacruz@gmail.com>
2021-06-05 17:06:44 +0800

    Reverted to previous format.

--- fccfrontendexercises/fccquotegenerator/src/components/QuoteGenerator.js
+++ fccfrontendexercises/fccquotegenerator/src/components/QuoteGenerator.js
@@ -8,14 +8,27 @@ const QuoteGenerator = () => {
     const [quotes, setQuotes] = useState(theQuotes);
     const generateQuoteIndex = Math.floor(Math.random() * quotes.length);
     return (
-        <Box backgroundColor={color} minH="100vh" padding="150px 400px 10px">
-            <Box backgroundColor="white" minH="300px"padding="50px">
-                <Box display="flex" flexDirection="column">
-                    <Box display="flex" flexDirection="column">
+        <Box backgroundColor={color}
+        minH="100vh"
+        padding="150px 400px 10px"
+        >
+            <Box 
+            backgroundColor="white"
+            minH="300px"
+            padding="50px"
+            >
+                <Box display="flex"
+                flexDirection="column"
+                >
+                    <Box 
+                    display="flex"
+                    flexDirection="column">
                         <Text fontSize="3xl" color={color}>{quotes[generateQuoteIndex].quote}</Text>
                         <Text alignSelf="flex-end" fontSize="md" padding="20px" color={color}>-{quotes[generateQuoteIndex].author}</Text>
                     </Box>
-                    <Box display="flex" justifyContent="space-between">
+                    <Box display="flex"
+                    justifyContent="space-between"
+                    >
                         <Box>
                             <Button margin="0px 5px" backgroundColor={color} color="white">Twt</Button>
                             <Button backgroundColor={color} color="white">Tum</Button>
